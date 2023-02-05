05ec7d88f3aa92a34fb6b404db7a89c2abd2435f
mbocian <michal.bocian@nokia.com>
2023-02-05 20:21:51 +0100

    much work - not done

--- screen_settings.py
+++ screen_settings.py
@@ -1,3 +1,4 @@
+from constants import colors, font_regular, font_bold
 from libqtile import bar, widget
 from libqtile.config import Screen
 
@@ -6,23 +7,90 @@ def init_screen():
     return Screen(
         top=bar.Bar(
             [
-                widget.GroupBox(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
+                widget.Sep(
+                    linewidth=0,
+                    padding=10,
+                    foreground=colors["accent"],
+                    background=colors["bg"],
                 ),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
+                widget.GroupBox(
+                    font=font_bold,
+                    margin_y=4,
+                    margin_x=0,
+                    padding_y=5,
+                    padding_x=5,
+                    borderwidth=1,
+                    spacing=5,
+                    toggle=True,
+                    use_mouse_wheel=False,
+                    urgent_alert_mode="border",
+                    urgent_border=colors["accent"],
+                    urgent_text=colors["accent"],
+                    active=colors["text"],
+                    inactive=colors["text"],
+                    this_current_screen_border=colors["accent"],
+                    this_screen_border=colors["bg"],
+                    other_screen_border=colors["bg"],
+                    other_current_screen_border=colors["bg"],
+                    rounded=False,
+                    highlight_method="line",
+                    foreground=colors["bg"],
+                    background=colors["bg"],
                 ),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.Spacer(
+                    length=bar.STRETCH,
+                ),
+                widget.Clock(
+                    background=colors["bg"],
+                    foreground=colors["accent"],
+                    font=font_regular,
+                    format="%a %d  %I:%M %p",
+                ),
+                widget.Spacer(
+                    length=bar.STRETCH,
+                ),
+                widget.Wlan(
+                    font=font_regular,
+                    interface="wlp0s20f3",
+                    format="{essid}: {percent:2.0%}",
+                    foreground=colors["accent"],
+                    background=colors["bg"],
+                    padding=5,
+                ),
+                widget.TextBox(
+                    font=font_regular,
+                    text='Vol:',
+                    foreground=colors["bg"],
+                    background=colors["accent"],
+                    padding=5,
+                ),
+                widget.Volume(
+                    font=font_regular,
+                    foreground=colors["bg"],
+                    background=colors["accent"],
+                    padding=5
+                ),
+                # widget.TextBox(
+                #     text='',
+                #     foreground=colors["accent"],
+                #     background=colors["bg"],
+                #     padding=0,
+                #     fontsize=40,
+                # ),
+                widget.TextBox(
+                    text='Battery: ',
+                    font=font_regular,
+                    foreground=colors["accent"],
+                    background=colors["bg"],
+                ),
+                widget.Battery(
+                    font=font_regular,
+                    background=colors["bg"],
+                    foreground=colors["accent"],
+                    low_foreground=colors["text"],
+                    notify_below=15,
+                    format="{percent: 2.0%}"
+                ),
             ],
             24,
         ),
