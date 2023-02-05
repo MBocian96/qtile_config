5c5739edfec27238a2375116ece90841dde989dd
mbocian <michal.bocian@nokia.com>
2023-02-05 20:21:01 +0100

    colors, fonts

--- constants.py
+++ constants.py
@@ -3,3 +3,12 @@ from libqtile.utils import guess_terminal
 mod = "mod4"
 terminal = guess_terminal()
 rofi = "rofi -show run"
+
+colors = {
+    "accent": "#98be65",
+    "bg": "#1c1f24",
+    "text": "#dfdfdf",
+}
+
+font_bold = "Ubuntu Bold"
+font_regular = "Ubuntu"
