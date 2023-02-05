3c7b82f99da6bc8d628a09d1468d0c8b8f21f42e
mbocian <michal.bocian@nokia.com>
2023-02-05 17:58:58 +0100

    old apps from previous config

---
+++ autostart.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+lxsession &
+picom --experimental-backends &
+volumeicon &
+com.brave.Browser &
+com.jetbrains.PyCharm-Professional &
+com.spotify.Client &
+com.microsoft.Teams &
+nautilus &
+pavucontrol &
+blueman-manager &
+playerctl &
+redshift -O 3400 &
+picom
