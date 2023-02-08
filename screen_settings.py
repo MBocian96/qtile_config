4a26387a57dcc39d71374a1ca0421aa801b75f0c
mbocian <michal.bocian@nokia.com>
2023-02-08 14:42:48 +0100

    two distinguished bars, colors, extras,

--- screen_settings.py
+++ screen_settings.py
@@ -3,7 +3,7 @@ from libqtile import bar, widget
 from libqtile.config import Screen
 
 
-def init_screen():
+def default_screen(external_widgets=()):
     return Screen(
         top=bar.Bar(
             [
@@ -15,7 +15,7 @@ def init_screen():
                 ),
                 widget.GroupBox(
                     font=font_bold,
-                    margin_y=4,
+                    margin_y=5,
                     margin_x=0,
                     padding_y=5,
                     padding_x=5,
@@ -25,7 +25,7 @@ def init_screen():
                     use_mouse_wheel=False,
                     urgent_alert_mode="border",
                     urgent_border=colors["accent"],
-                    urgent_text=colors["accent"],
+                    urgent_text=colors["text"],
                     active=colors["text"],
                     inactive=colors["text"],
                     this_current_screen_border=colors["accent"],
@@ -34,6 +34,7 @@ def init_screen():
                     other_current_screen_border=colors["bg"],
                     rounded=False,
                     highlight_method="line",
+                    highlight_color=[colors["bg"], colors["bg"]],
                     foreground=colors["bg"],
                     background=colors["bg"],
                 ),
@@ -51,34 +52,40 @@ def init_screen():
                     background=colors["bg"],
                     length=bar.STRETCH,
                 ),
+                *external_widgets,
                 widget.Wlan(
                     font=font_regular,
                     interface="wlp0s20f3",
                     format="{essid}: {percent:2.0%}",
                     foreground=colors["accent"],
                     background=colors["bg"],
-                    padding=5,
+                    padding=10,
+                ),
+                widget.TextBox(
+                    text='',
+                    foreground=colors["accent"],
+                    background=colors["bg"],
+                    padding=-7,
+                    fontsize=45,
                 ),
                 widget.TextBox(
                     font=font_regular,
                     text='Vol:',
                     foreground=colors["bg"],
                     background=colors["accent"],
-                    padding=5,
                 ),
                 widget.Volume(
                     font=font_regular,
                     foreground=colors["bg"],
                     background=colors["accent"],
-                    padding=5
                 ),
-                # widget.TextBox(
-                #     text='',
-                #     foreground=colors["accent"],
-                #     background=colors["bg"],
-                #     padding=0,
-                #     fontsize=40,
-                # ),
+                widget.TextBox(
+                    text='',
+                    background=colors["accent"],
+                    foreground=colors["bg"],
+                    padding=-7,
+                    fontsize=45,
+                ),
                 widget.TextBox(
                     text='Battery: ',
                     font=font_regular,
@@ -94,10 +101,67 @@ def init_screen():
                     format="{percent: 2.0%}"
                 ),
             ],
-            24,
+            20,
         ),
     )
 
 
+secondary_screen_additional_widgets = (
+    widget.CPU(
+        foreground=colors["accent"],
+        background=colors["bg"],
+    ),
+    widget.TextBox(
+        text='',
+        background=colors["bg"],
+        foreground=colors["accent"],
+        padding=-7,
+        fontsize=45,
+    ),
+    widget.Memory(
+        foreground=colors["bg"],
+        background=colors["accent"],
+        measure_mem="G"
+    ),
+    widget.TextBox(
+        text='',
+        background=colors["accent"],
+        foreground=colors["bg"],
+        padding=-7,
+        fontsize=45,
+    ),
+)
+
+main_screen_additional_widgets = (
+    widget.Notify(
+        background=colors["bg"],
+        foreground=colors["accent"],
+        audiofile='/usr/share/sounds/gnome/default/alerts/string.ogg'
+    ),
+    widget.TextBox(
+        text='',
+        background=colors["bg"],
+        foreground=colors["accent"],
+        padding=-7,
+        fontsize=45,
+    ),
+    widget.Pomodoro(
+        background=colors["accent"],
+        foreground=colors["bg"],
+        color_active=colors["bg"],
+        color_break=colors["bg"],
+        color_inactive=colors["bg"]
+    ),
+    widget.TextBox(
+        text='',
+        background=colors["accent"],
+        foreground=colors["bg"],
+        padding=-7,
+        fontsize=45,
+    ),
+)
+
+
 def init_screens():
-    return [init_screen(), init_screen(), init_screen(), ]
+    return [default_screen(), default_screen(main_screen_additional_widgets),
+            default_screen(secondary_screen_additional_widgets), ]
