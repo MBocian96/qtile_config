0ec1851dde540e35f340afe3e26dfc6baeaf9a7f
mbocian <michal.bocian@nokia.com>
2023-04-25 23:40:49 +0200

    removed pycharm

--- autostart.sh
+++ autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 picom &
 com.brave.Browser &
-com.jetbrains.PyCharm-Professional &
 com.spotify.Client &
 com.microsoft.Teams &
 nautilus &
 pavucontrol &
 blueman-manager &
 redshift -O 3400 &
+setxkbmap pl 
