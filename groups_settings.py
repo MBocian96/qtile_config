8ff80c8f7a58ac46891b3553e24a354e1a192210
mbocian <michal.bocian@nokia.com>
2023-12-13 11:29:07 +0100

    new keys

--- groups_settings.py
+++ groups_settings.py
@@ -10,19 +10,19 @@ from libqtile.config import Group, Key, Match
 
 def init_groups(keys: list[Key]):
     workspaces = [
-        Group("e", label="Prog", screen_affinity=2, matches=[Match(wm_class="jetbrains-pycharm")]),
-        Group("r", label="Net", screen_affinity=2, matches=[Match(wm_class="brave-browser")]),
-        Group("t", label="Teams", screen_affinity=2, matches=[Match(wm_class="microsoft teams - preview")]),
-        Group("1", label="File", screen_affinity=2, matches=[Match(wm_class="org.gnome.Nautilus")]),
-        Group("2", label="Misc", screen_affinity=2),
-        Group("3", label="Term", screen_affinity=1),
-        Group("4", label="Sett", screen_affinity=1,
+        Group("e", label="e: Prog",   screen_affinity=2, matches=[Match(wm_class="jetbrains-pycharm")]),
+        Group("r", label="r: Net",    screen_affinity=2, matches=[Match(wm_class="brave-browser")]),
+        Group("t", label="t: Teams",  screen_affinity=2, matches=[Match(wm_class="microsoft teams - preview")]),
+        Group("f", label="f: Term",   screen_affinity=1),
+        Group("g", label="g: File",   screen_affinity=2, matches=[Match(wm_class="org.gnome.Nautilus")]),
+        Group("z", label="z: Zim",    screen_affinity=2, matches=[Match(wm_class="Zim")]),
+        Group("x", label="x: Dscrd",  screen_affinity=2, matches=[Match(wm_class="discord")]),
+        Group("c", label="c: Slck",   screen_affinity=2, matches=[Match(wm_class="slack")]),
+        Group("v", label="v: Music",  screen_affinity=2, matches=[Match(wm_class="spotify")]),
+        Group("b", label="b: Sett",   screen_affinity=2,
               matches=[Match(wm_class="blueman-manager"), Match(wm_class="gnome-control-center"), Match(wm_class="Pavucontrol"),
                        Match(wm_class="pavucontrol")]),
-        Group("5", label="Music", screen_affinity=2, matches=[Match(wm_class="spotify")]),
-        Group("6", label="Misc", screen_affinity=2),
     ]
-
 #    keys += [Key([mod], "[", lazy.function(latest_group))]
 #    keys += [Key([mod], "]", lazy.function(latest_group))]
 
